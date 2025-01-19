7936cd0410b02e2fdbe69526d2fe9a1710b4cf8b
Иван <konkin@localhost.localdomain>
2025-01-19 21:20:25 +0300

    Добавил настройки для clojure запуск в контейнере

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,11 +29,25 @@ return {
                     ["symfony.enabled"] = true,
                 },
             })
-            lspconfig.clojure_lsp.setup({})
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+            lspconfig.clojure_lsp.setup({
+                cmd = {
+                    "docker", "exec", "-i", "clojure-dev", "clojure-lsp"
+                },
+            on_attach = function(client, bufnr)
+                -- Ключевые привязки для LSP
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+                end,
+                settings = {
+                    clojure = {
+                        lint = {
+                            enabled = true,  -- Включить линтер
+                        },
+                    },
+                },
+            })
         end
     }
 }
