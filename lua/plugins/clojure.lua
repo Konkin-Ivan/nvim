7d660931b87e1330ccd3c618626135dcdeb1f095
Иван <konkin@localhost.localdomain>
2025-01-19 21:37:34 +0300

    Добавил настройки для clojure запуск в контейнере

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -1,18 +1,20 @@
 return {
-    "Olical/conjure",
-    ft = { "clojure", },
-    lazy = true,
-    init = function()
-        -- Настройка автоматического запуска REPL
-        vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = true
-        vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "make start-repl"
-        -- vim.g["conjure#debug"] = true
-        -- Настройка сочетаний клавиш
-        vim.keymap.set('n', '<leader>cc', '<cmd>ConjureConnect<CR>', { silent = true })
-        vim.keymap.set('n', '<leader>ce', '<cmd>ConjureEvalCurrentForm<CR>', { silent = true })
-        vim.keymap.set('n', '<leader>cl', '<cmd>ConjureEvalCurrentLine<CR>', { silent = true })
-        vim.keymap.set('n', '<leader>cb', '<cmd>ConjureEvalBuf<CR>', { silent = true })
-        vim.keymap.set('n', '<leader>cs', '<cmd>ConjureSchool<CR>', { silent = true })
-    end,
+  "Olical/conjure",
+  ft = { "clojure" },
+  lazy = true,
+  init = function()
+    -- Отключаем поиск файла .nrepl-port и auto-repl
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
 
+    -- Указываем хост и порт вручную
+    vim.g["conjure#client#clojure#nrepl#connection#default_host"] = "localhost"
+    vim.g["conjure#client#clojure#nrepl#connection#default_port"] = 5555
+
+    -- Настройка сочетаний клавиш
+    vim.keymap.set('n', '<leader>cc', '<cmd>ConjureConnect<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>ce', '<cmd>ConjureEvalCurrentForm<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>cl', '<cmd>ConjureEvalCurrentLine<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>cb', '<cmd>ConjureEvalBuf<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>cs', '<cmd>ConjureSchool<CR>', { silent = true })
+  end,
 }
