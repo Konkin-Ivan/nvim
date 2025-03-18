6788c628ea7b303e06d6d3ea9579cfeb8c9b603e
Иван <konkin@localhost.localdomain>
2025-03-18 12:53:16 +0300

    Изменил настройки lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,83 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "gopls",
-                    "phpactor",
-                    "clojure_lsp"
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.gopls.setup({})
-            lspconfig.phpactor.setup({
-                init_options = {
-                    ["symfony.enabled"] = true,
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "phpactor",
+          "clojure_lsp"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Общие настройки для всех LSP
+      local on_attach = function(_, bufnr)
+        -- Ключевые привязки для LSP
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+      end
+
+      -- Условная загрузка LSP для Lua
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "lua",
+        callback = function()
+          lspconfig.lua_ls.setup({ on_attach = on_attach })
+        end,
+      })
+
+      -- Условная загрузка LSP для Go
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "go",
+        callback = function()
+          lspconfig.gopls.setup({ on_attach = on_attach })
+        end,
+      })
+
+      -- Условная загрузка LSP для PHP
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "php",
+        callback = function()
+          lspconfig.phpactor.setup({
+            on_attach = on_attach,
+            init_options = {
+              ["symfony.enabled"] = true,
+            },
+          })
+        end,
+      })
+
+      -- Условная загрузка LSP для Clojure
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "clojure",
+        callback = function()
+          lspconfig.clojure_lsp.setup({
+            cmd = { "clojure-lsp" },
+            on_attach = on_attach,
+            settings = {
+              clojure = {
+                lint = {
+                  enabled = true,  -- Включить линтер
                 },
-            })
-            lspconfig.clojure_lsp.setup({
-                cmd = {
-                    "docker", "exec", "-i", "clojure-dev", "clojure-lsp"
-                },
-            on_attach = function(client, bufnr)
-                -- Ключевые привязки для LSP
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
-                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
-                end,
-                settings = {
-                    clojure = {
-                        lint = {
-                            enabled = true,  -- Включить линтер
-                        },
-                    },
-                },
-            })
-        end
-    }
+              },
+            },
+          })
+        end,
+      })
+    end
+  }
 }
