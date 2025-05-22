80bd75c2cacb3f4749304708197d816c46fc8e17
Иван Конкин <konkin.ivan@yandex.ru>
2025-05-22 23:16:47 +0300

    Изменил порт для nREPL

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -3,12 +3,23 @@ return {
   ft = { "clojure" },
   lazy = true,
   init = function()
+    -- Настройки окна
+    vim.g["conjure#log#float#enabled"] = true
+    vim.g["conjure#log#hud#width"] = 1
+    vim.g["conjure#log#hud#height"] = 0.3
+    vim.g["conjure#log#wrap"] = true
+    vim.g["conjure#log#jump_to_latest#enabled"] = true
+    -- Автоматически открывать лог при выполнении кода
+    vim.g["conjure#log#auto_open"] = true
+    -- Закрывать HUD при переходе в нормальный режим
+    vim.g["conjure#log#hud#close_on_normal_mode"] = true
     -- Отключаем поиск файла .nrepl-port и auto-repl
     vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
 
     -- Указываем хост и порт вручную
-    vim.g["conjure#client#clojure#nrepl#connection#default_host"] = "172.24.0.2"
+    vim.g["conjure#client#clojure#nrepl#connection#default_host"] = "localhost"
     vim.g["conjure#client#clojure#nrepl#connection#default_port"] = 5555
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
 
     -- Настройка сочетаний клавиш
     vim.keymap.set('n', '<leader>cc', '<cmd>ConjureConnect<CR>', { silent = true })
@@ -16,5 +27,8 @@ return {
     vim.keymap.set('n', '<leader>cl', '<cmd>ConjureEvalCurrentLine<CR>', { silent = true })
     vim.keymap.set('n', '<leader>cb', '<cmd>ConjureEvalBuf<CR>', { silent = true })
     vim.keymap.set('n', '<leader>cs', '<cmd>ConjureSchool<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>ls', '<cmd>ConjureLogSplit<CR>') -- Открыть лог в split
+    vim.keymap.set('n', '<leader>lt', '<cmd>ConjureLogToggle<CR>') -- Переключить видимость
+    vim.keymap.set('n', '<leader>lq', '<cmd>ConjureLogCloseVisible<CR>') -- Закрыть все логи
   end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,8 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.zprint,        -- Lua
+                null_ls.builtins.formatting.cljstyle,      -- Clojure
                 -- для Руби
                 --null_ls.builtins.diagnostics.rubocop,
                 --null_ls.builtins.formatting.rubocop,
